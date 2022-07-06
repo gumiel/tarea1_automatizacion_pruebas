e737b0891bb1ee619370b318959272b4d317ed1c
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-07-05 23:42:44 -0400

    enunciado practica 1

---
+++ src/main/java/authentication/Authentication.java
@@ -0,0 +1,20 @@
+package authentication;
+
+public class Authentication {
+    private PermissionService permissionService;
+    private CredentialsService credentialsService;
+
+    public Authentication(){
+        permissionService=new PermissionService();
+        credentialsService= new CredentialsService();
+    }
+
+    public String login(String user, String pwd){
+        if (credentialsService.isValidCredential(user,pwd)){
+            String permission=permissionService.getPermission(user);
+            return "user authenticated successfully with permission: ["+permission+"]";
+        }else{
+            return "user or password incorrect";
+        }
+    }
+}
